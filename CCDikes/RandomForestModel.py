0bcad8912e0ccaa86bea02115550d78c6f2d222c
ElenSmi <eleni.smirniou@deltares.nl>
2023-10-02 17:25:49 +0200

    Added newest version of the code

--- CCDikes/RandomForestModel.py
+++ CCDikes/RandomForestModel.py
@@ -8,6 +8,9 @@ import pandas as pd
 import geopandas as gpd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import rasterio
+import numpy as np
+import shap
 
 
 def KNMI(start, end, station="348", interval='hour', variables=['RH', 'FF', 'RH']):
@@ -93,23 +96,36 @@ def check_meterological_data_per_point(punten, time_section):
     return punten
 
 
+def sample_dataframe_from_tif(dataframe, tiff_file):
+    # open the tif file
+    r = rasterio.open(tiff_file)
+    # create deep copy of the dataframe
+    values = dataframe.copy(deep=True)
+    # change the crs of the dataframe
+    values = values.to_crs("EPSG:28992")
+    # get the points
+    pts = np.array([values["geometry"].x, values["geometry"].y]).T
+    # get the values of the points
+    values_BAND = list(r.sample(pts))
+    # flatten the list
+    values_BAND = [item for sublist in values_BAND for item in sublist]
+    return values_BAND
 
-if __name__ == "__main__":
-    # read the geo database
-    path_to_database = "..\data\Digispectie2017_2022\Digispectie2017_2022.gdb"
-    layers = ['Inspectie2017_najaar_punt',
-              'Inspectie2018_najaar_punt',
-              'Inspectie2019_najaar_punt',
-              'Inspectie2020_voorjaar_punt',
-              'Inspectie2020_najaar_punt',
-              'Inspectie2021_voorjaar_punt',
-              'Inspectie2021_najaar_punt',
-              'Inspectie2022_voorjaar_punt']
-    # %%
+def sample_points_from_tif(pts, tiff_file):
+    # open the tif file
+    r = rasterio.open(tiff_file)
+    # get the values of the points
+    values_BAND = list(r.sample(pts))
+    # flatten the list
+    values_BAND = [item for sublist in values_BAND for item in sublist]
+    return values_BAND
+
+
+def preprocess_and_aggregate_data(database_path, layer_gpkg, add_satellite_data=False, time_section=30):
     punten = []
-    for layer in layers:
+    for layer in layer_gpkg:
         if "punt" in layer:
-            punten.append(gpd.read_file(path_to_database, layer=layer))
+            punten.append(gpd.read_file(database_path, layer=layer))
     punten = gpd.GeoDataFrame(pd.concat(punten, axis=0, sort=False))
     # %%
     punten["inspection_value"] = [int(value.split("_")[0]) for value in punten["WAARDE"].to_list()]
@@ -118,7 +134,7 @@ if __name__ == "__main__":
                                      dimensions=["OBJECT_NAAM", "PARAMETER_NAAM",  "WAARDE"],
                                      color="inspection_value",
                                      color_continuous_scale=px.colors.sequential.YlGnBu_r)
-    fig.show()
+    #fig.show()
     # keep only "grass" of OBJECT_NAAM
     filtered_gdf = punten[punten["OBJECT_NAAM"] == "gras"]
     # %%
@@ -126,7 +142,7 @@ if __name__ == "__main__":
                                      dimensions=["OBJECT_NAAM", "PARAMETER_NAAM", "WAARDE"],
                                      color="inspection_value",
                                      color_continuous_scale=px.colors.sequential.YlGnBu_r)
-    fig.show()
+    #fig2.show()
     # %%
     open_existing_file = True
     if open_existing_file:
@@ -136,8 +152,41 @@ if __name__ == "__main__":
             filtered_gdf = gdf
 
     else:
-            filtered_gdf = check_meterological_data_per_point(filtered_gdf, 30)
+            filtered_gdf = check_meterological_data_per_point(filtered_gdf, time_section)
+    # get also the year of the inspection
+    # year is not needed for the model as it has no influence on the model
+    #filtered_gdf["year"] = [date.year for date in filtered_gdf["DATUM"].to_list()]
+    filtered_gdf['aspect'] = sample_dataframe_from_tif(filtered_gdf, "../data/aspect_area.tif")
+    filtered_gdf['slope'] = sample_dataframe_from_tif(filtered_gdf, "../data/slope_area.tif")
+    # -9999 is no data value
+    # how many points have no data value
+    print("Check if there are points with no data value for the slope and aspect variables")
+    print(f"Number of points with no data value: {len(filtered_gdf[filtered_gdf['aspect'] == -9999])}")
+    print(f"Number of points with no data value: {len(filtered_gdf[filtered_gdf['slope'] == -9999])}")
+    # remove the points with no data value
+    filtered_gdf = filtered_gdf[filtered_gdf['aspect'] != -9999]
+    filtered_gdf = filtered_gdf[filtered_gdf['slope'] != -9999]
 
+    # add the satellite data
+    if add_satellite_data:
+        # read the satellite data from the csv file
+        satellite_data = pd.read_csv("../data/sentinel_2_data.csv")
+        # keep only the nvdi values
+        satellite_data = satellite_data[["ndvi", "x", "y"]]
+        # to geodataframe
+        satellite_data = gpd.GeoDataFrame(satellite_data, geometry=gpd.points_from_xy(satellite_data.x, satellite_data.y))
+        # set the crs of the satellite data to the crs of the filtered_gdf
+        satellite_data = satellite_data.set_crs("EPSG:4326")
+        # intersect the inspection points with the satellite data points and merge the data
+        filtered_gdf = gpd.sjoin_nearest(filtered_gdf, satellite_data, how="left")
+        # check if there are nan values in the ndvi column
+        print(f"Number of nan values in the ndvi column: {len(filtered_gdf[filtered_gdf['ndvi'].isna()])}")
+        # remove the nan values
+        filtered_gdf = filtered_gdf[filtered_gdf['ndvi'].notna()]
+        drop_columns = ["index_right", "x_right", "y_right"]
+        filtered_gdf = filtered_gdf.drop(drop_columns, axis=1)
+        # rename the columns
+        filtered_gdf = filtered_gdf.rename(columns={"x_left": "x", "y_left": "y"})
     full_dataset = filtered_gdf.copy()
     ## encode the categorical variables
     categorical_variables = ["OBJECT_NAAM",]
@@ -150,8 +199,9 @@ if __name__ == "__main__":
     # drop the variables
     filtered_gdf = filtered_gdf.drop(drop_variables, axis=1)
     output_labels = sorted(full_dataset['WAARDE'].unique().tolist())
+    return filtered_gdf, output_labels
 
-    # %%
+def perform_and_evaluate_random_forest(filtered_gdf, output_labels):
     ## split the data into train and test data
     outputs = ["inspection_value"]
     X_train, X_test, y_train, y_test = train_test_split(filtered_gdf.drop(outputs, axis=1), filtered_gdf[outputs], test_size=0.2, random_state=42)
@@ -163,16 +213,17 @@ if __name__ == "__main__":
     print(f"y_test: {y_test.shape}")
     # %%
     # Instantiate model with 1000 decision trees
-    rf = RandomForestClassifier(n_estimators = 1000, random_state = 42)# Train the model on training data
+    rf = RandomForestClassifier(n_estimators=1000, random_state=42)# Train the model on training data
     rf.fit(X_train, y_train)
     # calculate prediction accuracy for the initial model
     predictions = rf.predict(X_test) # Calculate the absolute errors
     # plot 1:1 line plot
     plt.hist(y_test.to_numpy())
     plt.hist(predictions)
-    plt.show()
+    #plt.show()
     # %%
-    ConfusionMatrixDisplay.from_predictions( y_test, predictions, display_labels=output_labels, cmap=plt.cm.Blues, normalize='true')
+    ConfusionMatrixDisplay.from_predictions(y_test, predictions, display_labels=output_labels, cmap=plt.cm.Blues, normalize='true')
+    #plt.show()
     accuracy = rf.score(X_test, y_test)
     print(f"Accuracy: {accuracy * 100.0}%")
     # %%
@@ -182,15 +233,128 @@ if __name__ == "__main__":
     # List of tuples with variable and importance
     feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
     # Sort the feature importances by most important first
-    feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+    feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
     # Print out the feature and importances
     [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+    # plot shap values
 
-    end = datetime.datetime.now()
-    start = end - datetime.timedelta(days=30)
-    df = KNMI(start=start, end=end, station="350", interval='day', variables=['TG', 'SQ', 'DR', 'RH', 'EV24'])
-    df.dropna(inplace=True)
-    df = df[['TG', 'SQ', 'DR', 'RH', 'EV24']]
-    df_sum = df.sum(axis=0)
+    explainer = shap.TreeExplainer(rf)
+    shap_values = explainer.shap_values(X_test)
+    # clean plots
+    plt.show()
+    plt.clf()
+    shap.summary_plot(shap_values, X_test, plot_type="bar")
+    plt.show()
+    return rf, X_train, X_test, y_train, y_test
+
+def plot_bbox_last_month(bbox):
+    # create a grid
+    x = np.linspace(bbox[0], bbox[2], 50)
+    y = np.linspace(bbox[1], bbox[3], 50)
+    xx, yy = np.meshgrid(x, y)
+    # collect meteo data of the last 30 days
+    time_section = 365
+    date = datetime.datetime.now()
+    # get the max and min dates
+    end = date
+    start = end - datetime.timedelta(days=time_section)
+    # get the data
+    results_meteo = KNMI(start=start, end=end, station="350", interval='day', variables=['TG', 'SQ', 'DR', 'RH', 'EV24'])
+    mean_meteo = results_meteo.mean()
+    # note that the data is the same for each point
+    OBJECT_NAAM = 10 # 10 is the code for grass
+    # get aspect and slope data
+    points = np.array([xx.flatten(), yy.flatten()]).T
+    aspect = sample_points_from_tif(points, "../data/aspect_area.tif")
+    slope = sample_points_from_tif(points, "../data/slope_area.tif")
+    # 28992 to 4326
+    import pyproj
+    transformer = pyproj.Transformer.from_crs("epsg:28992", "epsg:4326")
+    # transform the points
+    points = transformer.transform(points[:, 0], points[:, 1])
+
+    # create a dataframe as the one used for the model
+    df = pd.DataFrame({'OBJECT_NAAM': OBJECT_NAAM * np.ones(len(aspect)),
+                       'x': points[0],
+                       'y': points[1],
+                       'TG': mean_meteo['TG'] * np.ones(len(aspect)),
+                       'SQ': mean_meteo['SQ'] * np.ones(len(aspect)),
+                       'DR': mean_meteo['DR'] * np.ones(len(aspect)),
+                       'RH': mean_meteo['RH'] * np.ones(len(aspect)),
+                       'EV24': mean_meteo['EV24'] * np.ones(len(aspect)),
+                       'aspect': aspect,
+                       'slope': slope,
+                       })
+    # to geo dataframe
+    df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x, df.y))
+    # set the crs
+    df.crs = "EPSG:4326"
+    # read the ndvi data
+    ndvi = gpd.read_file("../data/ndvi.csv")
+    # set the geometry to the points x and y
+    ndvi = ndvi.set_geometry(gpd.points_from_xy(ndvi.y, ndvi.x))
+    # set the crs
+    ndvi.crs = "EPSG:4326"
+    # join the dataframes
+    df = gpd.sjoin_nearest(df, ndvi, how="left")
+    # columns to drop
+    drop_columns = ndvi.columns.tolist()
+    drop_columns.remove("ndvi")
+    drop_columns.remove("x")
+    drop_columns.remove("y")
+    drop_columns.append("x_right")
+    drop_columns.append("y_right")
+    drop_columns.append("index_right")
+    # drop the columns
+    df = df.drop(drop_columns, axis=1)
+    # rename x_left and y_left to x and y
+    df = df.rename(columns={"x_left": "x", "y_left": "y"})
+    # remove the points with no ndvi data that are nan
+    # remove the points with no aspect or slope data
+    df = df[df['aspect'] != -9999]
+    df = df[df['slope'] != -9999]
+    # get the predictions
+    predictions = rf.predict(df)
+    predictions = np.array(predictions)
+    # create a dataframe
+    df = pd.DataFrame({'x': df['y'], 'y': df['x'], 'predictions': predictions})
+    # plot the results in a mapbox plot
+    fig = px.scatter_mapbox(df, lat="y", lon="x", color="predictions", zoom=10, color_continuous_scale=px.colors.sequential.Rainbow)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.show()
+
+
+
+if __name__ == "__main__":
+    # read the geo database
+    path_to_database = "..\data\Digispectie2017_2022\Digispectie2017_2022.gdb"
+    layers = ['Inspectie2017_najaar_punt',
+              'Inspectie2018_najaar_punt',
+              'Inspectie2019_najaar_punt',
+              'Inspectie2020_voorjaar_punt',
+              'Inspectie2020_najaar_punt',
+              'Inspectie2021_voorjaar_punt',
+              'Inspectie2021_najaar_punt',
+              'Inspectie2022_voorjaar_punt']
+    # preprocess the data
+    add_satellite_data = True
+    filtered_gdf, output_labels = preprocess_and_aggregate_data(path_to_database,
+                                                                layers,
+                                                                add_satellite_data=add_satellite_data,
+                                                                time_section=30)
+    # perform and evaluate the random forest
+    rf, X_train, X_test, y_train, y_test = perform_and_evaluate_random_forest(filtered_gdf, output_labels)
+    # plot results in a contour plot of the area
+    bbox = [72404.1738, 385300.4088, 135175.194, 415041.17]
+    plot_bbox_last_month(bbox)
+
+
+
+
+
+
+
+
+
 
 
---
+++ CCDikes/collect_ndvi.py
@@ -0,0 +1,93 @@
+import datetime
+import pystac_client
+import planetary_computer
+import stackstac
+import xarray as xr
+import geopandas as gpd
+import pandas as pd
+
+BAND = {
+    "B02": "blue",
+    "B03": "green",
+    "B04": "red",
+    "B08": "nir",
+    "B09": "evaporation",
+    "B11": "swir1",
+    "SCL": "SCL",
+}
+
+
+def get_ndvi_on_date_bbox(bbox, date, inspection_points):
+    # get date
+    sub_date = date
+    last_month = date - datetime.timedelta(days=30)
+    datetime_string = f"{last_month.year}-{last_month.strftime('%m')}-{last_month.strftime('%d')}/{sub_date.year}-{sub_date.strftime('%m')}-{sub_date.strftime('%d')}"
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace,
+    )
+    search = catalog.search(
+        collections=[
+            "sentinel-2-l2a"
+        ],  # atmospherically corrected Surface Reflectances (SR)
+        bbox=bbox,
+        max_items=20,
+        datetime=datetime_string,  # "2020-01-01/2020-01-31",
+        query={"eo:cloud_cover": {"lt": 30}},
+    )
+    items = search.item_collection()
+    print(f"{len(items)} items found in catalog search.")
+    if len(items) > 0:
+        data = stackstac.stack(
+            items,
+            epsg=4326,  # if you want to cast the data to a common crs
+            assets=list(BAND.keys()),
+            bounds_latlon=bbox.tolist(),
+            sortby_date="desc",  # maybe you want to sort the data by date?
+        )
+        # calculate the median
+        median = data.median(dim="time").compute()
+        #median = data.chunk({'time': -1}).quantile(q=0.8, dim="time").compute()
+        # calculate the Normalised difference moisture index
+        red = median.sel({"band": "B04"})
+        nir = median.sel({"band": "B08"})
+        ndvi = (nir - red) / (red + nir)  # this is still a lazy Dask computation
+        #ndvi = ndvi.compute()  # this will actually run the computation
+        # sample the data at the inspection points
+        x = xr.DataArray(list(inspection_points[:,0]), dims='z')
+        y = xr.DataArray(list(inspection_points[:,1]), dims='z')
+        time_ndvi = ndvi.sel(x=x, y=y, method='nearest').to_dataframe(name="ndvi").reset_index()
+        return time_ndvi
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    path_to_database = "..\data\Digispectie2017_2022\Digispectie2017_2022.gdb"
+    layers = ['Inspectie2017_najaar_punt',
+              'Inspectie2018_najaar_punt',
+              'Inspectie2019_najaar_punt',
+              'Inspectie2020_voorjaar_punt',
+              'Inspectie2020_najaar_punt',
+              'Inspectie2021_voorjaar_punt',
+              'Inspectie2021_najaar_punt',
+              'Inspectie2022_voorjaar_punt']
+    punten = []
+    for layer in layers:
+        if "punt" in layer:
+            punten.append(gpd.read_file(path_to_database, layer=layer))
+    punten = gpd.GeoDataFrame(pd.concat(punten, axis=0, sort=False))
+    # set crs
+    punten.crs = "EPSG:28992"
+    # transform to latlon
+    punten = punten.to_crs("EPSG:4326")
+    # get x and y as columns
+    punten["x"] = punten.geometry.x
+    punten["y"] = punten.geometry.y
+    # get points as array [:,2]
+    inspection_points = punten[["x", "y"]].to_numpy()
+    bbox = punten.total_bounds
+    date_now = datetime.datetime.now()
+    ndvi = get_ndvi_on_date_bbox(bbox, date_now, inspection_points)
+    # save to csv
+    ndvi.to_csv("../data/ndvi.csv")
